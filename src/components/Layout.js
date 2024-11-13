ace7bb3d8dce91189a0de96f135db80c73c2d6da
Minh Phuoc <winnerhuynh1234@gmail.com>
2024-11-13 22:13:59 +0700

    Sửa Layout & thêm thư viện xem online

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar'; // Đảm bảo đường dẫn đúng
 import Footer from './Footer'; // Nếu có footer riêng
+import logo from '../images/features/mess.png'
+import '../styles/Layout.css';
 
 function Layout({ children }) {
   return (
     <div className="layout">
       <Navbar />
         <div className="content">{children}</div>
+        <a href='https://m.me/hmfuocc' className='mess'><img src={logo} alt='messenger' /></a>
       <Footer />
     </div>
   );
