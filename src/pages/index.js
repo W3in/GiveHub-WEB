0c6b2e6c4987d1a33c918c856a4b05538f5b75b2
Minh Phuoc <winnerhuynh1234@gmail.com>
2024-11-21 10:14:25 +0700

    Update sau khi xóa OtherDonate

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ import Stats from './Stats/Stats';
 import Donate from './Donate/Donate';
 import OnlineDonate from './Donate/OnlineDonate';
 import OfflineDonate from './Donate/OfflineDonate';
-import OtherDonate from './Donate/OtherDonate';
 import FAQ from './Contact/FAQ';
 import Support from './Contact/Support';
 import Login from './Authorization/Login';
@@ -23,7 +22,6 @@ export const routes = [
   { path: "/donate", element: <Donate /> },
   { path: "/donate/online", element: <OnlineDonate /> },
   { path: "/donate/offline", element: <OfflineDonate /> },
-  { path: "/donate/other", element: <OtherDonate /> },  
   { path: "/faq", element: <FAQ /> },
   { path: "/support", element: <Support /> },
   { path: "/login", element: <Login /> },
