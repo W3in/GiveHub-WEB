93d5ebda52d8e6846eabe535c609f3762fef575f
Minh Phuoc <winnerhuynh1234@gmail.com>
2024-11-22 21:53:11 +0700

    Thêm nút về đầu trang

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,46 @@
 // src/components/Layout.js
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Navbar from './Navbar/Navbar'; // Đảm bảo đường dẫn đúng
 import Footer from './Footer'; // Nếu có footer riêng
 import logo from '../images/features/mess.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Layout.css';
 
 function Layout({ children }) {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+  
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // Cuộn mượt
+    });
+  };
+
   return (
     <div className="layout">
       <Navbar />
         <div className="content">{children}</div>
         <a href='https://m.me/hmfuocc' className='mess'><img src={logo} alt='messenger' /></a>
+        <div>
+          {isVisible && (
+            <button onClick={scrollToTop} className="back-to-top">
+              <FontAwesomeIcon icon={faArrowUp} />
+            </button>
+          )}
+        </div>
       <Footer />
     </div>
   );
